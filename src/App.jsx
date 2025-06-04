0d542eef9b35489ebae75ff8bca908f5fb816f78
gaurab_017 <gaurabghimire1234@gmail.com>
2025-06-04 09:47:05 +0545

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>Learning React || Gaurab Ghimire</h1>
+  )
+}
+
+export default App
